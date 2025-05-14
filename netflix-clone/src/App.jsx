7594257fe8b8c13d794b1f5109a09cc27897148b
dagabate <dagtfsm@gmail.com>
2025-05-13 21:43:52 -0400

    Just first commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="">Hello</h1>
+    </>
+  );
+}
+
+export default App;
