0e0b66c192ff8ee36524cac2afea274f7dd0a81d
dagabate <dagtfsm@gmail.com>
2025-05-14 23:48:22 -0400

    Banner and edited header and footer

---
+++ netflix-clone/src/Components/Banner/Banner.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import axios from "../../Utils/axios.jsx";
+import requests from "../../Utils/requests";
+import "./Banner.css";
+function Banner() {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(requests.fetchHorrorMovies);
+        setMovie(
+          request.data.results[
+            Math.floor(Math.random() * request.data.results.length)
+          ]
+        );
+      } catch (error) {
+        console.error("Error fetching Netflix Originals:", error);
+      }
+    })();
+  }, []);
+
+  // Truncate function
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        <div className="banner_buttons">
+          <button className="banner_button play">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+
+        <h1 className="banner_description">{truncate(movie?.overview, 100)}</h1>
+      </div>
+      <div className="banner_fade_bottom"></div>
+    </header>
+  );
+}
+
+export default Banner;
--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../Components/Header/Header.jsx";
 import Footer from "../../Components/Footer/Footer.jsx";
+import Banner from "../../Components/Banner/Banner.jsx";
 
 function Home() {
   return (
     <>
       <Header />
+      <Banner/>
       <Footer />
     </>
   );
